41330610cbe225e85b8ac24ad92420b509d6ad1a
Oluwatobi <oldwhisper98@icloud.com>
2025-07-06 16:00:20 -0700

    created ls-tree

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 #[allow(unused_imports)]
 use std::env;
+use std::ffi::CStr;
 #[allow(unused_imports)]
 use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
 use anyhow::Context;
 use anyhow::Ok;
-use bytes::buf;
 use flate2;
 use std::io::BufReader;
 
@@ -47,7 +47,7 @@ fn main() -> Result<(), anyhow::Error> {
         f.read_to_string(&mut content)?;
         let content_len =content.len();
         let content_to_encode = format!("blob {}\0{}", content_len, content);
-        let mut m = sha1_smol::Sha1::from(&content_to_encode).digest().to_string();
+        let m = sha1_smol::Sha1::from(&content_to_encode).digest().to_string();
         let obj_folder = &m[..2];
         let obj_file = &m[2..];
         let obj_path = format!(".git/objects/{}/{}", obj_folder, obj_file);
@@ -61,6 +61,58 @@ fn main() -> Result<(), anyhow::Error> {
         print!("{}",m);
 
     } 
+    else if args[1] == "ls-tree" {
+        let tree_hash: &str;
+        let mut name_only = false;
+        if args[2] == "--name-only" {
+            tree_hash = &args[3];
+            name_only = true;
+        }
+        else {
+            tree_hash = &args[2];
+        }
+        print!("{}, {}", tree_hash, name_only);
+        let tree_folder = &tree_hash[..2];
+        let tree_h = &tree_hash[2..];
+        let path = format!(".git/objects/{}/{}", tree_folder, tree_h);
+        println!("{}", &path);
+        let file = File::open(&path).expect("could not open file");
+        let d = flate2::read::ZlibDecoder::new(file);
+        let mut d = BufReader::new(d);
+        let mut header= Vec::new();
+        d.read_until(0, &mut header)?;
+        let header_s = CStr::from_bytes_with_nul(&header)?;
+        let header_s = header_s.to_str()?;
+        print!("{}\n", header_s);
+        loop {
+            let mut entry = Vec::new();
+            let n = d.read_until(0, &mut entry)?;
+            if n == 0 {
+                break;
+            }
+            if entry.is_empty() {
+                break;
+            }
+            entry.pop(); // remove trailing null byte
+            let entry_str = String::from_utf8_lossy(&entry);
+            let entry_vec: Vec<&str> = entry_str.split(" ").collect();
+            let mode = &entry_vec[0];
+            let file_name = &entry_vec[1];
+            let mut sha = [0u8; 20];
+            if let Err(_) = d.read_exact(&mut sha) {
+                break;
+            }
+            let sha_hex = sha.iter().map(|b| format!("{:02x}", b)).collect::<String>();
+            if name_only {
+                print!("{}\n", file_name);
+            }
+            else {
+                print!("{} {} {}\n", mode, file_name, sha_hex);
+            }
+        }
+
+
+    }
     else {
         println!("unknown command: {}", args[1])
     }
