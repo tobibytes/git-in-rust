2f2ce9d6e8eed254408adc6a684eab4156323a05
Oluwatobi <oldwhisper98@icloud.com>
2025-07-05 22:40:43 -0700

    decoding git objects

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::env;
+use std::ffi::CStr;
 use std::io::stdout;
 #[allow(unused_imports)]
 use std::fs;
@@ -31,9 +32,18 @@ fn main() -> Result<(), anyhow::Error> {
         let file = File::open(&path).expect("could not open file");
         let d = ZlibDecoder::new(file);
         let mut d = BufReader::new(d);
-        let mut s = String::new();
-        d.read_to_string(&mut s).context("reading from git objects")?;
-        print!("{}", s);
+        let mut header_s = Vec::new();
+        let mut full_string = String::new();
+        d.read_until(0, &mut header_s)?;
+        let header = CStr::from_bytes_with_nul(&header_s)
+        .expect("know there is exactly one nul, and it's at the end");
+        let header = header
+        .to_str()
+        .context(".git/objects file header isn't valid UTF-8")?;
+        d.read_to_string(&mut full_string).context("reading from git objects")?;
+        let header_len = header.len();
+        let body = &full_string[header_len..];
+        print!("{:?}", body);
   
         
        }
