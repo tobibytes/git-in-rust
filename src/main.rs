cc4619a585fd2b0fb085a1c16552ab8c2944af87
Oluwatobi <oldwhisper98@icloud.com>
2025-07-06 16:01:55 -0700

    created ls-tree

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,7 @@ fn main() -> Result<(), anyhow::Error> {
         let mut full_string = String::new();
         d.read_until(0, &mut header)?;
         d.read_to_string(&mut full_string).context("reading from git objects")?;
-        print!("{}", full_string);
+        // print!("{}", full_string);
        }
     }
     else if args[1] == "hash-object" {
@@ -71,7 +71,6 @@ fn main() -> Result<(), anyhow::Error> {
         else {
             tree_hash = &args[2];
         }
-        print!("{}, {}", tree_hash, name_only);
         let tree_folder = &tree_hash[..2];
         let tree_h = &tree_hash[2..];
         let path = format!(".git/objects/{}/{}", tree_folder, tree_h);
